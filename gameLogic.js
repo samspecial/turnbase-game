36a53421496ea7bbc92a8c17e3b3ef7ea5662d39
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-04-24 22:59:53 +0100

    Moving players on the board

--- gameLogic.js
+++ gameLogic.js
@@ -11,29 +11,82 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
-const players = ["player", "player1"]
-const weapons = ["weapon", "weapon1", "weapon2", "weapon3"]
-let isOccupied = ""
+const weapons = [{
+    name: 'weapon1',
+    power: 10,
+    img: ''
+}, {
+    name: 'weapon2',
+    power: 10,
+    img: ''
+}, {
+    name: 'weapon3',
+    power: 10,
+    img: ''
+}, {
+    name: 'weapon4',
+    power: 10,
+    img: ''
+}]
+const player = [{
+    name: 'player1',
+    lifepoint: 100,
+    weapon: {
+        name: 'weapon',
+        power: 10,
+        img: ''
+    }, location: {
+        row: 0,
+        column: 0
+    }
+}, {
+    name: 'player2',
+    lifepoint: 100,
+    weapon: {
+        name: 'weapon',
+        power: 10,
+        img: ''
+    }, location: {
+        row: 0,
+        column: 0
+    }
+}]
+const barrier = { name: "figure" }
 function placeItem(item) {
+    let isOccupied = ""
     let row = randomNumber(1, 9);
-    let col = randomNumber(1, 9);
-    let selectedSquare = $(`[data-rows=${row}][data-columns=${col}]`);
+    let column = randomNumber(1, 9);
+    let selectedSquare = $(`[data-rows=${row}][data-columns=${column}]`);
     isOccupied = selectedSquare.hasClass('occupied') //return true or false
 
     if (isOccupied) {
         return placeItem(item);
     } else {
-        selectedSquare.addClass(item).addClass('occupied');
+        item['location'] = { row: row, column: column }
+        selectedSquare.addClass(item['name']).addClass('occupied');
     }
 }
+// function placeBarrier(item) {
+//     let isOccupied = ""
+//     let row = randomNumber(1, 9);
+//     let col = randomNumber(1, 9);
+//     let selectedSquare = $(`[data-rows=${row}][data-columns=${col}]`);
+//     isOccupied = selectedSquare.hasClass('occupied') //return true or false
+
+//     if (isOccupied) {
+//         return placeBarrier(item);
+//     } else {
+//         selectedSquare.addClass(item['name']).addClass('occupied');
+//     }
+// }
 function renderBarriers() {
     for (var i = 0; i < 12; i++) {
-        placeItem('figure');
+        placeItem(barrier);
     }
 }
 function renderPlayers() {
-    for (var i = 0; i < players.length; i++) {
-        placeItem(players[i]);
+    for (var i = 0; i < player.length; i++) {
+        placeItem(player[i]);
     }
 }
 function renderWeapons() {
@@ -45,27 +98,27 @@ renderBarriers();
 renderPlayers();
 renderWeapons();
 
-//Write a function to move Player
-// Identify the Player and select the players
-// Determine the current position
-// Determine the distance/length
-function movePlayer() {
-
-    isOccupied = $('.square').not('.occupied', '.figure', '.player1')
-
-    if (!isOccupied) {
-        alert("There is an obstruction")
-        return movePlayer()
+let currentPlayer = player[0]
+function switchPlayer() {
+    if (currentPlayer === player[0]) {
+        currentPlayer = player[1];
     } else {
-        $(isOccupied).click(function () {
-            $('.player').animate({
-                left: "-=60px",
-
-            }).animate({
-                right: "+=60px"
-            })
-            console.log("good")
-        })
+        currentPlayer = player[0];
     }
 }
-movePlayer()
+
+function movePlayer(newRow, newColumn) {
+    let playerRow = newRow;
+    let playerColumn = newColumn;
+    let currentPlayerRow = currentPlayer['location']['row']
+    let currentPlayerColumn = currentPlayer['location']['column']
+    $(`[data-rows=${currentPlayerRow}][data-columns=${currentPlayerColumn}]`).removeClass(currentPlayer['name']);
+    $(`[data-rows=${playerRow}][data-columns=${playerColumn}]`).addClass(currentPlayer['name']);
+    currentPlayer['location']['row'] = playerRow;
+    currentPlayer['location']['column'] = playerColumn;
+    switchPlayer();
+}
+
+$('#gameBoard').on('click', function () {
+    movePlayer(event.target.dataset.rows, event.target.dataset.columns)
+})
