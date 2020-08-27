90395c3a5b5e99b557ed239197dcf0ca07a8edea
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-08-27 10:43:05 +0100

    fix pickweapon

--- gameLogic.js
+++ gameLogic.js
@@ -214,6 +214,7 @@ function checkBarriers(squareRow, squareCol) {
 // Add the weapon in the square to the player
 
 const pickWeapon = (i, j, weapons) => {
+
     for (let x = 0; x < weapons.length; x++) {
         let selectWeaponSquare = $(`[data-rows=${i}][data-columns=${j}]`)
         let weaponPosition = selectWeaponSquare.hasClass(weapons[x]['name']);
